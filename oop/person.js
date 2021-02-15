f2bfb386f38fdd10cd9ff7d8cead277e1030315e
max-b10 <bungaymax@gmail.com>
2021-02-15 13:11:46 +0000

    Updating to Class syntax

--- oop/person.js
+++ oop/person.js
@@ -1,37 +1,37 @@
-const Person = function (firstName, lastName, age, likes = []) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-  this.likes = likes;
-};
-
-Person.prototype.getBio = function () {
-  let bio = `${this.firstName} is ${this.age}.`;
-  this.likes.forEach((like) => {
-    bio += ` ${this.firstName} likes ${like}.`;
-  });
-  return bio;
-};
-
-Person.prototype.setName = function (fullName) {
-  const names = fullName.split(" ");
-  this.firstName = names[0];
-  this.lastName = names[1];
-};
+class Person {
+  constructor(firstName, lastName, age, likes = []) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.likes = likes;
+  }
+  getBio() {
+    let bio = `${this.firstName} is ${this.age}.`;
+    this.likes.forEach((like) => {
+      bio += ` ${this.firstName} likes ${like}.`;
+    });
+    return bio;
+  }
+  setName(fullName) {
+    const names = fullName.split(" ");
+    this.firstName = names[0];
+    this.lastName = names[1];
+  }
+}
 
 const me = new Person("Bob", "Smith", 50, ["eating", "sleeping"]);
 
-me.getBio = function () {
-  return "this is fake";
-};
+// me.getBio = function () {
+//   return "this is fake";
+// };
 
 me.setName("Baz Turner");
 console.log(me.getBio());
 
 const person2 = new Person("Bill", "Stone", 20);
 
-Person.prototype.getBio = function () {
-  return "Testing Testing";
-};
+// Person.prototype.getBio = function () {
+//   return "Testing Testing";
+// };
 
 console.log(person2.getBio());
